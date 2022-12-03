3ab929c556641a6a3a7f8e34106e7682bbd47e16
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-12-03 13:21:11 +0300

    Add additional task

---
+++ task_recursia/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+12345 -> 5 */
+
+int count = 0;
+void DigitCount(int number) 
+{
+    if (number == 0)
+    {
+        return;
+    }
+    count++;
+    DigitCount(number/10);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+DigitCount(number);
+Console.Write($"Количество цифр в числе: {count}");
